f7f29eb27b186b9dbf65e0d81b6a181a61374656
Banee Ishaque K <Baneeishaque@gmail.com>
2017-11-10 21:03:53 +0530

    Sales patch

---
+++ android/get_sales_person_upcoming_works.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'config.php';
+
+$sales_person_id = filter_input(INPUT_POST, 'sales_person_id');
+
+$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works` WHERE `sales_person_id`=$sales_person_id AND `work_date` = DATE_FORMAT( CONVERT_TZ( NOW( ) , '-05:30', '+00:00' ),'%Y-%m-%d')";
+//echo $status_sql;
+
+$status_result = $con->query($status_sql);
+
+$emptyarray = array();
+
+if (mysqli_num_rows($status_result) != 0) {
+    array_push($emptyarray, array("status" => "0"));
+
+    while ($status_row = mysqli_fetch_assoc($status_result)) {
+        $emptyarray[] = $status_row;
+    }
+} else {
+    array_push($emptyarray, array("status" => "1"));
+}
+
+echo json_encode($emptyarray);
+
+
