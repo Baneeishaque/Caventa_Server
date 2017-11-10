454924acca19d5f265013bec4ce5a011f1a75c80
Banee Ishaque K <Baneeishaque@gmail.com>
2017-11-10 12:27:07 +0530

    Server change

--- android/config.php
+++ android/config.php
@@ -1,5 +1,5 @@
 <?php
- $con = new mysqli("localhost", "filebirb_ndk", "9895204814", "filebirb_caventa");
+ // $con = new mysqli("localhost", "filebirb_ndk", "9895204814", "filebirb_caventa");
 //$con = new mysqli("localhost", "edcccet_ndk", "9895204814", "edcccet_lottery");
 //$con=new mysqli("localhost","root","","filebirb_lottery");
-//$con = new mysqli("localhost", "vfmobo6d_ndk", "9895204814", "vfmobo6d_caventa");
+$con = new mysqli("localhost", "vfmobo6d_ndk", "9895204814", "vfmobo6d_caventa");
