1f95120a89abe09a6ee43ba12132a881e8259525
karan <karan@DESKTOP-LL2QPFP>
2017-11-11 17:17:47 +0530

    George

--- android/get_pending_works.php
+++ android/get_pending_works.php
@@ -2,9 +2,8 @@
 
 include_once 'config.php';
 
-$sales_person_id = filter_input(INPUT_POST, 'sales_person_id');
-
-$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works` WHERE `sales_person_id`=$sales_person_id AND `status`=1";
+$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works` WHERE `work_date` = DATE_FORMAT( CONVERT_TZ( NOW( ) , '-05:30', '+00:00' ),'%Y-%m-%d')";
+//$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works`";
 //echo $status_sql;
 
 $status_result = $con->query($status_sql);
--- android/get_sales_person_upcoming_works.php
+++ android/get_sales_person_upcoming_works.php
@@ -4,7 +4,7 @@ include_once 'config.php';
 
 $sales_person_id = filter_input(INPUT_POST, 'sales_person_id');
 
-$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works` WHERE `sales_person_id`=$sales_person_id AND `work_date` = DATE_FORMAT( CONVERT_TZ( NOW( ) , '-05:30', '+00:00' ),'%Y-%m-%d')";
+$status_sql = "SELECT `id`, `name`, `address`, `work_date`, `status`, `sales_person_id` FROM `works` WHERE `sales_person_id`=$sales_person_id AND `work_date` > DATE_FORMAT( CONVERT_TZ( NOW( ) , '-05:30', '+00:00' ),'%Y-%m-%d')";
 //echo $status_sql;
 
 $status_result = $con->query($status_sql);
