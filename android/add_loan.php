24dc7cea0f5ba6ea76cf29fd410ee7a906243860
baneeishaque <baneeishaque@gmail.com>
2017-11-30 03:46:43 +0530

    add_loan_installments

--- android/add_loan.php
+++ android/add_loan.php
@@ -3,12 +3,11 @@
 include_once 'config.php';
 
 $description = filter_input(INPUT_POST, 'description');
-$amount = filter_input(INPUT_POST, 'amount');
 $loan_amount = filter_input(INPUT_POST, 'loan_amount');
 $installment_amount= filter_input(INPUT_POST, 'installment_amount');
 
 // $sql = "INSERT INTO `works`(`name`, `address`, `work_date`,`insertion_date_time`, `sales_person_id`) VALUES ('$work_name','$work_address','$work_date',CONVERT_TZ(NOW(),'-05:30','+00:00'),$sales_person_id)";
-$sql="INSERT INTO `loans`(`id`, `insertion_date_time`, `description`, `loan_amount`, `installment_amount`) VALUES (CONVERT_TZ(NOW(),'-05:30','+00:00'),'$description',$amount,$loan_amount,$installment_amount)";
+$sql="INSERT INTO `loans`( `insertion_date_time`, `description`, `loan_amount`, `installment_amount`) VALUES (CONVERT_TZ(NOW(),'-05:30','+00:00'),'$description',$loan_amount,$installment_amount)";
 //$bill_id_query = "SELECT MAX(id) AS id from works";
 //$bill_no_result = $con->query($bill_id_query);
 //
--- android/add_loan_installments.php
+++ android/add_loan_installments.php
@@ -1,12 +1,15 @@
 <?php
 
 include_once 'config.php';
-
+$loan_id= filter_input(INPUT_POST, 'loan_id');
 $receipt_number = filter_input(INPUT_POST, 'receipt_number');
-$amount = filter_input(INPUT_POST, 'amount');
-
+$payed_amount = filter_input(INPUT_POST, 'payed_amount');
+$principle = filter_input(INPUT_POST, 'principle');
+$intrest = filter_input(INPUT_POST, 'intrest');
+$remarks = filter_input(INPUT_POST, 'remarks');
 // $sql = "INSERT INTO `works`(`name`, `address`, `work_date`,`insertion_date_time`, `sales_person_id`) VALUES ('$work_name','$work_address','$work_date',CONVERT_TZ(NOW(),'-05:30','+00:00'),$sales_person_id)";
-$sql="INSERT INTO `investments`( `insertion_date_time`, `description`, `amount`) VALUES (CONVERT_TZ(NOW(),'-05:30','+00:00'),'$description',$amount)";
+//$sql="INSERT INTO `investments`( `insertion_date_time`, `description`, `amount`) VALUES (CONVERT_TZ(NOW(),'-05:30','+00:00'),'$description',$amount)";
+$sql="INSERT INTO `loan_installments`(`loan_id`, `insertion_date_time`, `receipt_number`, `payed_amount`, `principle`, `intrest`, `remarks`) VALUES ($loan_id,CONVERT_TZ(NOW(),'-05:30','+00:00'),$receipt_number,$payed_amount,$principle,$intrest,$remarks)";
 //$bill_id_query = "SELECT MAX(id) AS id from works";
 //$bill_no_result = $con->query($bill_id_query);
 //
