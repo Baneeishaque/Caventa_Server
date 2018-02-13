22f9b784e84162686170055164e430741a7466eb
dk <dk@dk-PC>
2018-02-13 09:09:27 +0530

    URL Fix

--- android/config.php
+++ android/config.php
@@ -1,5 +1,11 @@
 <?php
- // $con = new mysqli("localhost", "filebirb_ndk", "9895204814", "filebirb_caventa");
+
+$cpanel_username = "findandg";
+//echo $cpanel_username;
+//$con = new mysqli("localhost", "filebirb_ndk", "9895204814", "filebirb_caventa");
 //$con = new mysqli("localhost", "edcccet_ndk", "9895204814", "edcccet_lottery");
 //$con=new mysqli("localhost","root","","filebirb_lottery");
-$con = new mysqli("localhost", "vfmobo6d_ndk", "9895204814", "vfmobo6d_caventa");
+//$con = new mysqli("localhost", "vfmobo6d_ndk", "9895204814", "vfmobo6d_account_ledger");
+//$con = new mysqli("localhost", "findandg_ndk", "aA9895204814", "findandg_account_ledger");
+$con = new mysqli("localhost", $cpanel_username . "_ndk", "aA9895204814", $cpanel_username . "_caventa");
+//echo $cpanel_username . "_ndk";
